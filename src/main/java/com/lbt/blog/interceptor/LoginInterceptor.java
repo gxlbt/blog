39c2736142bff6f1f1a900a2eae9765813c64344
lbt <johndoe@example.com>
2019-08-28 15:51:42 +0800

    登录拦截

--- src/main/java/com/lbt/blog/interceptor/LoginInterceptor.java
+++ src/main/java/com/lbt/blog/interceptor/LoginInterceptor.java
@@ -1,11 +1,11 @@
-package com.lbt.blog.intercept;
+package com.lbt.blog.interceptor;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginIntercept extends HandlerInterceptorAdapter {
+public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (request.getSession().getAttribute("user") == null){
--- src/main/java/com/lbt/blog/interceptor/WebConfig.java
+++ src/main/java/com/lbt/blog/interceptor/WebConfig.java
@@ -1,12 +1,13 @@
-package com.lbt.blog.intercept;
+package com.lbt.blog.interceptor;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
+@Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginIntercept()).addPathPatterns("/admin/**")
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin").excludePathPatterns("/admin/login");
         super.addInterceptors(registry);
     }
